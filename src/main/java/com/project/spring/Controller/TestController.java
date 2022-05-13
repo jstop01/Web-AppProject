04002a48ef1229cc964c6e351bd74568b10515df
jstop01 <jstop01@naver.com>
2022-05-13 23:07:11 +0900

    jsp 설정값 추가 및 기본 테스트

--- src/main/java/com/project/spring/Controller/TestController.java
+++ src/main/java/com/project/spring/Controller/TestController.java
@@ -2,22 +2,19 @@ package com.project.spring.Controller;
 
 import com.project.spring.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController
+@Controller
 public class TestController {
 
-    @Autowired
-    private TestService testService;
-
-    @GetMapping("/test/test")
-    List<Map<String, Object>> getBoard () {
-
-        return testService.getBoard();
+    @GetMapping("/hello")
+    public String testPage() {
+        return "hello";
     }
 
 }
