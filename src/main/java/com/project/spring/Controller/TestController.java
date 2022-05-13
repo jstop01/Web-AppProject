2261204f6c3349aa96fecb0f9d6745465f515cbb
jstop01 <jstop01@naver.com>
2022-05-14 05:42:18 +0900

    템플릿 엔진 타입리프로 변경

--- src/main/java/com/project/spring/Controller/TestController.java
+++ src/main/java/com/project/spring/Controller/TestController.java
@@ -3,6 +3,7 @@ package com.project.spring.Controller;
 import com.project.spring.service.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,9 +13,10 @@ import java.util.Map;
 @Controller
 public class TestController {
 
-    @GetMapping("/hello")
-    public String testPage() {
-        return "hello";
+    @GetMapping("/login")
+    public String testPage(Model model) {
+        model.addAttribute("test","테스트입니다");
+        return "login";
     }
 
 }
